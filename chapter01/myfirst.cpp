b8e18359d034e2e628464bc2d491433deccf6a2a
selfTransition <lwt6077@gmail.com>
2024-01-05 08:40:08 +0800

    feat(application):cout

---
+++ chapter01/myfirst.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // a preprocessor directive 预处理指令
+
+int main() 
+{   // function header
+    using namespace std; // make definitions visible
+    cout << "Come up and C++ me some time."; // output
+    cout << endl; // start a new line
+    cout << "You won't regret it!" << endl; // output
+    return 0; // terminate main()
+}
