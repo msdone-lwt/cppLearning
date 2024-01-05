23c8b0a46de5c7f1992d5db84a12feabc57b6dda
selfTransition <lwt6077@gmail.com>
2024-01-05 18:00:02 +0800

    feat(application):cout、cin 练习

---
+++ chapter01/carrots.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int carrots;
+    carrots = 25;
+    cout << "I have " << carrots << " carrots." << endl;
+    carrots--;
+    cout << "Crunch, crunch, Now I have  " << carrots << " carrots." << endl;
+    return 0;
+}
