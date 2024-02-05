e1dc28a5fb71a3feeddcbe9e4c7ad0255ac7d0ee
selfTransition <lwt6077@gmail.com>
2024-02-05 17:52:43 +0800

    feat(application):char

---
+++ chapter03/chartype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+    using namespace std;
+    char ch; // declare a char variable
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "hola! ";
+    cout << "Thank you for the " << ch << " character." << endl;
+    return 0;
+}
